354e8b9faf86d9422803210e8fb9be789ec343e6
chayashivalingegowda <41785270+chayashivalingegowda@users.noreply.github.com>
2023-07-25 11:52:26 +0530

    Create np2.cpp

---
+++ np2.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+void modify(int *arr)
+{
+    rotate(arr,arr+3,arr+5);
+    rotate(arr,arr+2,arr+4);
+    
+}
+int main() {
+    // Write C++ code here
+int iarr[5];
+for(int i=0;i<5;++i)
+*(iarr+i)=i*2;
+modify(iarr);
+for(int i=0;i<5;++i)
+
+cout<<*(iarr+i)<<" ";
+
+
+    return 0;
+}
